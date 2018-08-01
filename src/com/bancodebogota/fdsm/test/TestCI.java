a5d52e7a5599adb9d30e086d55875ccdcc004fcb
Alejandro <Alejandro@Alejo>
2018-07-31 20:25:50 -0500

    Commit Inicial CI

---
+++ src/com/bancodebogota/fdsm/test/TestCI.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bancodebogota.fdsm.test;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class TestCI {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public int sum(int n1, int n2)
+    {
+        return n1 +n2;
+    }
+    
+    public static void main(String[] args) {
+        System.err.println("TESTCI");
+    }
+    
+}
