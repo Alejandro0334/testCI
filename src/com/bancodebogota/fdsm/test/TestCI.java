aaee1adfec74405f44fd95a3b90b8e7e4feeb514
Alejandro <Alejandro@Alejo>
2018-08-04 09:25:00 -0500

    Commit validarSonar

--- src/com/bancodebogota/fdsm/test/TestCI.java
+++ src/com/bancodebogota/fdsm/test/TestCI.java
@@ -17,7 +17,13 @@ public class TestCI {
     
     public int sum(int n1, int n2)
     {
+        
+        int n3 = n1+n2;
+        int n4 = n1+n2;
+        int n4 = n1+n2;
+        
         return n1 +n2;
+        
     }
     
     public static void main(String[] args) {
